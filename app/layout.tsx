85c8d4e6edd1b6fa0e4d17143dbbcd1321ae2586
Alterius <therealsamchaney@gmail.com>
2024-04-23 14:30:05 -0500

    Fxied curly brace vertical placement with min-h-[85vh] on outer div in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,14 +19,12 @@ export default function RootLayout({
         <RouteColorHandler />
         <Nav />
         {/* Need to hide overflow due to brackets going outside page bounds */}
-        <div className="">
-        <div className="flex scrollbar-hide flex-nowrap">
+        <div className="flex scrollbar-hide min-h-[85vh]">
           {/* <div className=" bg-darkTextPrimary mt-9">TEST</div> */}
           <div className="gradient-text frame-brace left hidden lg:block sticky pl-2 top-[30lvh]">{"{"}</div>
           <main className="w-full p-6">{children}</main>
           <img id="logo" src="/sam_chaney_logo.svg" className="gradient-text absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 h-[800px] z-[-1]" />
           <div className="gradient-text frame-brace right hidden lg:block sticky pr-2 top-[30vh]">{"}"}</div>
-          </div>
           </div>
       </body>
     </html>
