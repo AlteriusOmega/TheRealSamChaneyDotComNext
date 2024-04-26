b54adf34035e6b1f294e86ffcf1098cef46ed206
AlteriusOmega <TheRealSamChaney@gmail.com>
2024-04-25 19:40:30 -0500

    Temporarily fixed music square bracket displaying differently on Android since it is a rare unicode character and thus rendered by the OS, not the font. Did this by just setting different margin bottoms for small screen size and lower. Need to check how it looks on iOS

--- app/coding/CodingNav.tsx
+++ app/coding/CodingNav.tsx
@@ -4,15 +4,16 @@ import Link from "next/link";
 const CodingNav: React.FC = () => {
   return (
     <div className="flex items-center sticky top-[39.5vh] lg:ml-[-40px] ml-3">
-      <div className="gradient-text text-[10rem] ml-[-40px] mr-[-30px] mb-[-20px]">𝄕</div>    
-      <div className="flex flex-col items-center">
+      <div className="gradient-text text-[10rem] ml-[-40px] mr-[-30px]">𝄕</div>
+      {/* Make margin bottom bigger for below small screen because 𝄕 is rendered differently on Android */}
+      <div className="flex flex-col items-center mb-14 sm:mb-5">
         <h1 className="text-l">Projects:</h1>
         <Link href="/coding/this-site ">ThisSite</Link>
         <Link href="/coding/geometry-generator ">GeoGen</Link>
         <Link href="/coding/bebop">Bebop.js</Link>
         <Link href="/coding/beat-cube">BeatCube</Link>
         </div>
-      <div className="gradient-text text-[10rem] scale-x-[-1] ml-[-30px] mb-[-20px] mr-5">𝄕</div>
+      <div className="gradient-text text-[10rem] scale-x-[-1] ml-[-30px] mr-5">𝄕</div>
       </div>
   );
 };
